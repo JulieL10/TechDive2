7022cbbdd42d5e7b504a19731d5e7c3eaa7c3975
Miguel-Val <chahpo@gmail.com>
2024-02-10 00:49:49 -0500

    Created backend with routes

---
+++ api/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const examRoutes = require('./routes/exams')
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+
+
+// routes
+app.use('/api/exams', examRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=> {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log(`connected to db & listening on port ${process.env.PORT}`)
+        });
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+
+
+
