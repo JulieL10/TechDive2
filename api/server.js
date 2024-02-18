a84d85aa72bf087bd9149159702fac8a409dd608
Miguel-Val <chahpo@gmail.com>
2024-02-18 12:14:15 -0500

    Added cors to serverjs

--- api/server.js
+++ api/server.js
@@ -3,10 +3,12 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const examRoutes = require('./routes/exams')
+const cors = require('cors');
+
 
 // express app
 const app = express()
-
+app.use(cors());
 // middleware
 app.use(express.json())
 
