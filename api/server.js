6e623e343e78578efed0b9c88da444eb3de9e8ef
Miguel-Val <chahpo@gmail.com>
2024-02-28 16:58:38 -0500

    fixing api links

--- api/server.js
+++ api/server.js
@@ -7,7 +7,9 @@ const cors = require('cors');
 
 // express app
 const app = express()
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000", "https://medreport.onrender.com"]
+}));
 // middleware
 app.use(express.json())
 
