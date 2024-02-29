41f418e811ae752e92ec67325430031437189c16
Darren Harris <theharriscode401@gmail.com>
2024-02-28 21:45:40 -0500

    changed allowed origin link

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 // express app
 const app = express()
 app.use(cors({
-    origin: ["http://localhost:3000", "https://medreport.onrender.com"]
+    origin: ["http://localhost:3000", "https://techdive2-wpc8.onrender.com/"]
 }));
 // middleware
 app.use(express.json())
