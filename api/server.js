ac5de4867feccacd4b7bebc5f5ad76b0f239db4e
Miguel-Val <chahpo@gmail.com>
2024-02-29 11:03:34 -0500

    updated cors for deployment

--- api/server.js
+++ api/server.js
@@ -7,7 +7,10 @@ const cors = require('cors');
 
 // express app
 const app = express()
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000/", "https://medreport.onrender.com/"]
+}));
+
 // middleware
 app.use(express.json())
 
